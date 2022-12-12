d483925dad009b202a755ffcbc6ac1af51b1bc6e
Asad Chohan <asadchauhan906@gmail.com>
2022-12-12 14:02:40 +0500

    fix issues

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
-from flask import Flask, request
+from flask import Flask, render_template
 app = Flask(__name__)
-
-
-
-
 
 @app.route("/")
 def hello():
-   print (request.headers)
-   return "Hi World!"
-
-
+   return render_template('index.html')
 
 if __name__ == "__main__":
    app.run()
-
-
